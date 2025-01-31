d81167a3b531b021b17dc2907bfce072aaac9e5b
Igor <igorlimaisl7@gmail.com>
2025-01-31 15:35:17 -0300

    add

---
+++ adicao_inteiros.py
@@ -0,0 +1,15 @@
+print("Arme e efetue as questões )")
+print("A) 243 + 235:")
+print("B) 345 + 35:")
+print("C) 1.256 + 34:")
+print("D) 3.345 + 34.567:")
+
+a = 243 + 235 
+b = 345 + 35
+c = 1.256 + 34
+d = 3.454 + 34.567
+
+print(a)
+print(b)
+print(c)
+print(d)
